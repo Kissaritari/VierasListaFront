56298c5e3cda871a26311819bb9df29bc1c18011
Kissaritari <tiitiainnin@hotmail.com>
2025-06-21 15:54:42 +0300

    connect db

--- src/hooks/useDatabase.tsx
+++ src/hooks/useDatabase.tsx
@@ -1,18 +1,21 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const useDatabase = () => {
-  const supabaseUrl = "https://okhmiydbagubyoeheoso.supabase.co";
-  const supabaseKey = process.env.SUPABASE_ANON_KEY;
-  if (!supabaseKey) {
-    throw new Error(
-      "Supabase key is not defined. Please set the SUPABASE_KEY environment variable."
-    );
-  }
-  const supabase = createClient(supabaseUrl, supabaseKey);
+const supabaseUrl = "https://okhmiydbagubyoeheoso.supabase.co";
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || process.env.SUPABASE_ANON_KEY;
 
+if (!supabaseKey) {
+  throw new Error(
+    "Supabase key is not defined. Please set the SUPABASE_KEY environment variable."
+  );
+}
+
+// Create a single instance of the Supabase client
+export const supabase = createClient(supabaseUrl, supabaseKey);
+
+export const useDatabase = () => {
   // Function to add an item to a specified table
   const addItem = async (table: string, item: Record<string, any>) => {
-    const { data, error } = await supabase.from(table).insert([item]);
+    const { data, error } = await supabase.from(table).insert([item]).select();
     if (error) {
       throw error;
     }
